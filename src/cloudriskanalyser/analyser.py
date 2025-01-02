21299d93ac4c8f208613590838339ea58f07b864
Oliver Freund <mail@oliver-freund.ch>
2025-01-02 17:42:57 -0500

    Added function to verify the user's input

--- src/cloudriskanalyser/analyser.py
+++ src/cloudriskanalyser/analyser.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+import re
+import sys
 
 
 #################################
@@ -8,12 +10,40 @@
 #################################
 # Shared Functions
 #################################
+def is_valid_csp(csp_url: str) -> bool:
+    # verify if the string is technically a valid hostname
+    if is_valid_hostname(csp_url):
+        return True
+    else:
+        return False
+
+    # Include content-wise check if the application is acutally a CSP
+
+
+def is_valid_hostname(hostname: str) -> bool:
+    if len(hostname) > 255:
+        return False
+    if "." not in hostname:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[:-1]  # strip exactly one dot from the right, if present
+    allowed = re.compile(r"(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
+
 
 #################################
 # Main
 #################################
 def main():
-    print("Hello World!")
+    # accept user input
+    print("Welcome to CloudRiskAnalyser")
+    application_url = input("Please enter the FQDN of a cloud storage service which you would like to assess: ")
+
+    if (is_valid_csp(application_url)):
+        print(application_url + " is a valid cloud storge service.")
+    else:
+        print(application_url + " is no valid cloud storage service. Please try again.")
+        sys.exit()
 
 
 if __name__ == "__main__":
--- tests/test_cloudriskanalyser.py
+++ tests/test_cloudriskanalyser.py
@@ -1,5 +1,25 @@
-import cloudriskanalyser  # noqa: F401
+# import pytest
+
+# Own modules
+import cloudriskanalyser.analyser as cra
 
 
-def test_always_passes():
-    assert True
+def test_function_is_valid_csp():
+
+    # www.dropbox.com is a valid CSP, result "True" expected
+    if cra.is_valid_csp("www.dropbox.com"):
+        assert True
+    else:
+        assert False
+
+    # www.wikipedia.org is no valid CSP, result "False" expected
+    # if not cra.is_valid_csp("www.wikipedia.org"):
+    #     assert True
+    # else:
+    #    assert False
+
+    # flup is no valid FQDN, result "False" expected
+    if not cra.is_valid_csp("flup"):
+        assert True
+    else:
+        assert False
